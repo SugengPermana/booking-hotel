a99b84f28dc110603c7c884a8b8124e05508bda1
Sugeng Permana Desembry <mylistensong6@gmail.com>
2025-11-13 22:14:28 +0700

    input data amenities

--- components/admin/room/create-form.tsx
+++ components/admin/room/create-form.tsx
@@ -5,8 +5,9 @@ import { IoCloudUploadOutline, IoTrashOutline } from "react-icons/io5";
 import Image from "next/image";
 import { BarLoader } from "react-spinners";
 import { string } from "zod";
+import { Amenities } from "@prisma/client";
 
-const CreateForm = () => {
+const CreateForm = ({amenities}: {amenities:Amenities[]}) => {
   const inputFileRef = useRef<HTMLInputElement>(null);
   const [image, setImage] = useState("");
   const [message, setMessage] = useState("");
@@ -74,15 +75,20 @@ const CreateForm = () => {
             </div>
           </div>
           <div className="mb-4 grid md:grid-cols-3">
+            {amenities.map((item)=> (
+            <div className="flex items-center mb-4" key={item.id}>
             <input
               type="checkbox"
               name="amenities"
+              defaultValue={item.id}
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
               placeholder="Chekbox"
             />
             <label className="ms-2 text-sm font-medium text-gray-900 capitalize">
-              Spa
+              {item.name}
             </label>
+            </div>
+            ))}
             <div aria-live="polite" aria-atomic="true">
               <span className="text-sm text-red-500 mt-2">Message</span>
             </div>
