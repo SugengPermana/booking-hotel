b553ac88a5345a92ef32418267861831704dcb94
Sugeng Permana Desembry <mylistensong6@gmail.com>
2025-11-15 23:15:12 +0700

    style room detail page

--- types/room.ts
+++ types/room.ts
@@ -1,5 +1,19 @@
 import { Prisma } from "@prisma/client";
 
 export type RoomProps = Prisma.RoomGetPayload<{
-  include: {ROomAmenities: {select: {amidenitiesid: true}}}
-}>
+  include: { ROomAmenities: { select: { amidenitiesid: true } } };
+}>;
+
+export type RoomDetailProps = Prisma.RoomGetPayload<{
+  include: {
+    ROomAmenities: {
+      include: {
+        amenities: {
+          select: {
+            name: true;
+          };
+        };
+      };
+    };
+  };
+}>;
