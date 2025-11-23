4c5c6e4601937c269f43a5d08c2b8083d0ec7d54
Sugeng Permana Desembry <mylistensong6@gmail.com>
2025-11-23 13:53:12 +0700

    Update dashboard cards to fetch and display revenue, reserve, and customer data

--- components/dashboard-cards.tsx
+++ components/dashboard-cards.tsx
@@ -1,15 +1,24 @@
 import { LuChartArea, LuShoppingCart, LuUser, LuUsers } from "react-icons/lu";
+import { getRevenueAndReserve, getTotalCustomer } from "@/lib/data";
+import { formatCurrency } from "@/lib/utils";
+import { notFound } from "next/navigation";
 
-const DashboardCards = () => {
+const DashboardCards =async () => {
+  const [data, customer] = await Promise.all([
+    getRevenueAndReserve(),
+    getTotalCustomer(),
+  ])
+  if(!data || !customer) return notFound();
+
   return (
-    <div className="grid md:grid-cols-3 gap-5 pb-10">
+    <div className="grid md:grid-cols-3 gap-5 pb-10 mt-5">
       <div className="flex items-center bg-white border rounded-md overflow-hidden shadow-sm">
         <div className="p-4 bg-green-400">
           <LuChartArea className="text-white size-12" />
         </div>
         <div className="px-4 text-gray-700">
           <h3 className="text-sm tracking-wider"> Total Revenue</h3>
-          <p className="text-3xl">9999</p>
+          <p className="text-3xl">{formatCurrency(data.revenue)}</p>
         </div>
       </div>
       <div className="flex items-center bg-white border rounded-md overflow-hidden shadow-sm">
@@ -18,7 +27,7 @@ const DashboardCards = () => {
         </div>
         <div className="px-4 text-gray-700">
           <h3 className="text-sm tracking-wider"> Total Reservation</h3>
-          <p className="text-3xl">9999</p>
+          <p className="text-3xl">{data.reserve}</p>
         </div>
       </div>
       <div className="flex items-center bg-white border rounded-md overflow-hidden shadow-sm">
@@ -27,7 +36,7 @@ const DashboardCards = () => {
         </div>
         <div className="px-4 text-gray-700">
           <h3 className="text-sm tracking-wider"> Total Costumers</h3>
-          <p className="text-3xl">9999</p>
+          <p className="text-3xl">{customer.length}</p>
         </div>
       </div>
     </div>
