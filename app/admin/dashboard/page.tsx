5a47aa133f948fe84a5c842af17a2942a18e618f
Sugeng Permana Desembry <mylistensong6@gmail.com>
2025-11-23 13:25:01 +0700

    import dashboard card to dashborad

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,10 +1,20 @@
+import DashboardCards from "@/components/dashboard-cards"
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+const metadata: Metadata = {
+  title: "Dashboard",
+}
 
 const DashboardPage = () => {
   return (
-    <div className='py-20 mt-10'> Dashbaord Page
-      
+    <div className='max-w-screen-xl px-4 py-16 mt-10 mx-auto'> Dashbaord Page
+      <h1 className="text-4xl font-bold text-gray-800">Dashboard</h1>
+      <Suspense>
+      <DashboardCards />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
 export default DashboardPage
---
+++ components/dashboard-cards.tsx
@@ -0,0 +1,37 @@
+import { LuChartArea, LuShoppingCart, LuUser, LuUsers } from "react-icons/lu";
+
+const DashboardCards = () => {
+  return (
+    <div className="grid md:grid-cols-3 gap-5 pb-10">
+      <div className="flex items-center bg-white border rounded-md overflow-hidden shadow-sm">
+        <div className="p-4 bg-green-400">
+          <LuChartArea className="text-white size-12" />
+        </div>
+        <div className="px-4 text-gray-700">
+          <h3 className="text-sm tracking-wider"> Total Revenue</h3>
+          <p className="text-3xl">9999</p>
+        </div>
+      </div>
+      <div className="flex items-center bg-white border rounded-md overflow-hidden shadow-sm">
+        <div className="p-4 bg-green-400">
+          <LuShoppingCart className="text-white size-12" />
+        </div>
+        <div className="px-4 text-gray-700">
+          <h3 className="text-sm tracking-wider"> Total Reservation</h3>
+          <p className="text-3xl">9999</p>
+        </div>
+      </div>
+      <div className="flex items-center bg-white border rounded-md overflow-hidden shadow-sm">
+        <div className="p-4 bg-green-400">
+          <LuUsers className="text-white size-12" />
+        </div>
+        <div className="px-4 text-gray-700">
+          <h3 className="text-sm tracking-wider"> Total Costumers</h3>
+          <p className="text-3xl">9999</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardCards;
